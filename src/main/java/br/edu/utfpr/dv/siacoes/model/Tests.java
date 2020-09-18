13f56dfbaa45bea66ee2d2cd72344dff18665258
unknown <gabrielantalsilva@gmail.com>
2020-09-17 21:52:18 -0300

    Feito o primeiro teste com os métodos setFullName e getFullName, onde eu declar uma String para ser armazenada neles e depois fazer o comparativo com o dado da variável compar através do assertEquals

--- src/main/java/br/edu/utfpr/dv/siacoes/model/Tests.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/model/Tests.java
@@ -7,9 +7,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class Tests {
 	
-	
-
+	@Test
+	void DepartamentTests() {
+		Department dep = new Department();
+		
+		dep.setFullName("Jack Sparrow");
+		
+		String capitain = dep.getFullName();
+		String compar= "Jack Sparow";
+		
+		assertEquals(compar, capitain);
 		
 	}
+
 
 }
